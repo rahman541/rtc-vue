e03c88273c4358743dd755841bf6d41516ac22fe
Rahman <rahmanromli@yahoo.com>
2017-11-09 06:05:34 +0800

    Rename file

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -16,7 +16,7 @@ window.Vue = require('vue');
  */
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
-Vue.component('chat', require('./components/ChatComponent.vue'));
+Vue.component('chat-message', require('./components/ChatMessage.vue'));
 
 const app = new Vue({
     el: '#app'
