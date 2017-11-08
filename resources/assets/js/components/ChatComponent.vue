7f1950af59769119fa1f9baaf84c096bd361d854
Rahman <rahmanromli@yahoo.com>
2017-11-09 06:01:08 +0800

    Add chat component & it generated asset by laravel mi

---
+++ resources/assets/js/components/ChatComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Chat Component</div>
+
+                    <div class="panel-body">
+                        Chat
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Chat component mounted.')
+        }
+    }
+</script>
---
+++ resources/views/chat.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Chat VueJS</title>
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+	<div id="app">
+		<h1>Chat App</h1>
+		<chat></chat>
+	</div>
+	<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
